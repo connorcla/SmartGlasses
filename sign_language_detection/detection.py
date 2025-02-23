8011994fab6043ca187cf8fa95e74265fcc8982a
Katswato <ujicofluff@gmail.com>
2025-02-22 17:32:19 -0800

    Modularizing default asl detection a bit

--- sign_language_detection/detection.py
+++ sign_language_detection/detection.py
@@ -5,6 +5,9 @@ import os
 import torch
 import torchvision
 from torchvision import transforms, datasets
+
+from pathlib import Path
+from PIL import Image
 import matplotlib.pyplot as plt
 
 
@@ -86,10 +89,10 @@ test_dataset = torch.utils.data.Subset(test_dataset, indices[:split])
 
 len(train_dataset), len(test_dataset)
 
-
-
-if __name__ == '__main__':
-
+def RunTraining():
+    
+    training_version = input("Input training version (ex: 1.0.0): ")
+    
     train_dataloader = torch.utils.data.DataLoader(dataset=train_dataset,
                                                 batch_size=batch_size, 
                                                 shuffle=True, 
@@ -103,10 +106,16 @@ if __name__ == '__main__':
 
     classes = train_dataloader.dataset.dataset.classes
 
-
-
+    print("\n--------------------\n")
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    if (torch.cuda.is_available()):
+        print(f"Using gpus: {torch.cuda.device_count()} GPUs available...")
+    else:
+        print(f"Using cpu...")
+    print("\n--------------------\n")
+    print()
 
+    print("\n--------------------\n")
     model = torchvision.models.resnet50(pretrained=True)
 
     for param in model.parameters():
@@ -121,7 +130,12 @@ if __name__ == '__main__':
 
 
     model.to(device)
+    print("\n--------------------\n")
+    print()
 
+    
+    print("\n--------------------")
+    print("Running epochs...\n")
     for epoch in range(num_epoch):  
         running_loss = 0
         correct_train = 0
@@ -131,7 +145,6 @@ if __name__ == '__main__':
         for images, labels in train_dataloader:
             images = images.to(device)
             labels = labels.to(device)
-
             
             output = model(images)
             loss = criterion(output, labels)
@@ -139,27 +152,25 @@ if __name__ == '__main__':
             correct_train += (torch.max(output, dim=1)[1] == labels).sum()
             total_train += labels.size(0)
 
-            
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
             running_loss += loss.item()
 
-        print(f"Epoch {epoch+1} completed")
+        print(f"Epoch {epoch+1} completed...s")
 
 
         # Save model after each epoch
-        torch.save(model, f'checkpoint_{epoch + 1}.pth')
+        torch.save(model, f'checkpoints/{training_version}/checkpoint_{epoch + 1}.pth')
+    print("\n--------------------\n")
+    print("Training finished\n")
 
+    return device, model, classes 
+    
 
-    from pathlib import Path
-    from PIL import Image
-
-
+def PlotTraining(device, model, classes):
     test_data_path = Path('./datasets/asl-alphabet/asl_alphabet_test/')
-
-
     test_dataset = ASLTestDataset(test_data_path, transforms=test_transforms)
 
     columns = 7
@@ -180,9 +191,33 @@ if __name__ == '__main__':
         ax[i][j].axis('off')
         j += 1
         if j == columns:
-            j = 0
-            i += 1
+                j = 0
+                i += 1
             
     plt.show()
+    return
+
+
+
+        
+if __name__ == '__main__':
+
+    user_choice = ""
+    device = model = classes = ""
+
+    while (user_choice != "Quit"):
+
+        print("-----\nMenu\n-----")
+        print("1. Training")
+        print("2. Plot\n")
+        user_choice = input("Choice: ")
+
+        if (user_choice == "Training"):
+            device, model, clases = RunTraining()
+        elif (user_choice == "Plot"):
+            PlotTraining()
+        else:
+            break
+
 
 
