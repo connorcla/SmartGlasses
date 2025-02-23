e8e22af0c91170952dbed1c667056069cf5c5f7e
iracema clark <emilyclark5293@gmail.com>
2025-02-23 12:14:23 -0800

    switched to mobilenet_v2

--- sign_language_detection/detection.py
+++ sign_language_detection/detection.py
@@ -4,36 +4,20 @@ import os
 
 import torch
 import torchvision
-from torchvision import transforms, datasets
+from torchvision.transforms import v2
+from torchvision import datasets
+from torch.utils.data import random_split
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
-
-
-class ASLTestDataset(torch.utils.data.Dataset):
-    def __init__(self, root_path, transforms=None):
-        super().__init__()
-        
-        self.transforms = transforms
-        self.imgs = sorted(list(Path(root_path).glob('*.jpg')))
-        
-    def __len__(self):
-        return len(self.imgs)
-    
-    def __getitem__(self, idx):
-        img_path = self.imgs[idx]
-        img = Image.open(img_path).convert('RGB')
-        
-        label = img_path.parts[-1].split('_')[0]
-        if self.transforms:
-            img = self.transforms(img)
-        
-        return img, label
  
+
+### setting datapaths for image datasets
 
 dataset_path = "./datasets/asl_alphabet"
 
 if not os.path.exists(dataset_path):
-    raise FileNotFoundError(f"Dataset not found at {dataset_path}. Please download it first.")
+    raise FileNotFoundError(f"Dataset not found at {dataset_path}")
 
 print("Dataset found at:", dataset_path)
 
@@ -51,138 +35,145 @@ print("Dataset paths are correct!")
 """
 
 
-test_size = 0.2
+### preprocessing dataset
+
+train_transforms = v2.Compose([
+    v2.Resize(size=(128,128), antialias=True),
+    v2.RandomHorizontalFlip(p=0.5),
+    v2.ToTensor(),
+    v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+validation_transforms = v2.Compose([
+    v2.Resize(size=(128,128), antialias=True),
+    v2.ToTensor(),
+    v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+train_split_percentage = 0.8
+
+full_dataset = datasets.ImageFolder(train_data_path, transform=train_transforms)
+
+full_dataset_size = len(full_dataset)
+train_size = int(full_dataset_size * train_split_percentage)
+validation_size = full_dataset_size - train_size
+
+train_subset, validation_subset = random_split(full_dataset, [train_size, validation_size])
+
+train_subset.dataset.transform = train_transforms
+validation_subset.dataset.transform = validation_transforms
+
+
+### defining parameters for training
+
 batch_size = 32
-num_epoch = 5
+num_epochs = 10
 learning_rate = 0.001
 num_classes = 29
 
-train_transforms = transforms.Compose([
-    transforms.Resize(128),
-    transforms.ToTensor()
-])
+train_dataloader = torch.utils.data.DataLoader(dataset=train_subset, batch_size=batch_size, shuffle=True)
+validation_dataloader = torch.utils.data.DataLoader(dataset=validation_subset, batch_size=batch_size, shuffle=False)
 
-test_transforms = transforms.Compose([
-    transforms.Resize(128), 
-    transforms.ToTensor()
-])
+classes = train_dataloader.dataset.dataset.classes
 
+model = torchvision.models.mobilenet_v2(pretrained=True)
 
-train_dataset = datasets.ImageFolder(train_data_path, transform=train_transforms)
-num_train_samples = len(train_dataset)
-train_dataset
+for param in model.parameters():
+    param.requires_grad = False
 
-test_dataset = datasets.ImageFolder(train_data_path, transform=test_transforms)
-test_dataset
+in_features = model.classifier[1].in_features
+model.classifier[1] = torch.nn.Linear(in_features, num_classes)
 
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.classifier[1].parameters(), lr=learning_rate)
 
-torch.manual_seed(1)
-indices = torch.randperm(num_train_samples)
-
-split = int(num_train_samples * test_size)
-
-train_dataset = torch.utils.data.Subset(train_dataset, indices[split:])
-test_dataset = torch.utils.data.Subset(test_dataset, indices[:split])
-
-len(train_dataset), len(test_dataset)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.to(device)
 
 
+### training model
 
-if __name__ == '__main__':
+print("Starting training...")
 
-    train_dataloader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                                batch_size=batch_size, 
-                                                shuffle=True, 
-                                                num_workers=0)
+for epoch in range(num_epochs):  
+    running_loss_train = 0
+    correct_train = 0
+    total_train = 0
 
-    test_dataloader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                                batch_size=batch_size,
-                                                shuffle=False,
-                                                num_workers=0)
+    # training loop
+    model.train()
+    progress_bar = tqdm(train_dataloader, desc=f"Epoch {epoch+1}/{num_epochs}", leave=False)
+    for images, labels in train_dataloader:
+        images, labels = images.to(device), labels.to(device)
 
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
 
-    classes = train_dataloader.dataset.dataset.classes
+        loss.backward()
+        optimizer.step()
 
+        running_loss_train += loss.item()
+        correct_train += (torch.max(outputs, dim=1)[1] == labels).sum()
+        total_train += labels.size(0)
 
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    model = torchvision.models.resnet50(pretrained=True)
-
-    for param in model.parameters():
-        param.requires_grad = False
-
-    in_features = model.fc.in_features
-    model.fc = torch.nn.Linear(in_features, num_classes)
-    model
-
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-
-    model.to(device)
-
-    for epoch in range(num_epoch):  
-        running_loss = 0
-        correct_train = 0
-        total_train = 0
-
-        model.train()
-        for images, labels in train_dataloader:
-            images = images.to(device)
-            labels = labels.to(device)
-
+        progress_bar.update(1)
             
+    train_accuracy = (correct_train / total_train) * 100
+    avg_loss_train = running_loss_train / len(train_dataloader)
+
+    """
+    # validation loop
+    model.eval()
+    running_loss_validation = 0
+    correct_validation = 0
+    total_validation = 0
+
+    with torch.no_grad():
+        for images, labels in validation_dataloader:
+            images, labels = images.to(device), labels.to(device)
+
             output = model(images)
-            loss = criterion(output, labels)
+            loss = criterion(outputs, labels)
 
-            correct_train += (torch.max(output, dim=1)[1] == labels).sum()
-            total_train += labels.size(0)
+            running_loss_validation += loss.item()
+            correct_validation += (torch.max(outputs, dim=1)[1] == labels).sum().item()
+            total_validation += labels.size(0)
+    
+    validation_accuracy = (correct_validation / total_validation) * 100
+    avg_loss_validation = running_loss_validation / len(validation_dataloader)    
+    """
+    
+    # epoch results
+    print(f"Epoch {epoch+1}/{num_epochs} completed")
+    print("Avg training loss: {avg_loss_train:.4f}, Training accuracy: {train_accuracy:.2f}%")
+    #print("Avg validation loss: {avg_loss_validation:.4f}, Validation accuracy: {validation_accuracy:.2f}%\n")
 
-            
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            running_loss += loss.item()
-
-        print(f"Epoch {epoch+1} completed")
-
-
-        # Save model after each epoch
-        torch.save(model, f'checkpoint_{epoch + 1}.pth')
-
-
-    from pathlib import Path
-    from PIL import Image
+    # Save model after each epoch
+    torch.save(model.state_dict(), f'mobilenet_asl_epoch{epoch + 1}.pth')
 
 
-    test_data_path = Path('./datasets/asl-alphabet/asl_alphabet_test/')
 
+### final testing on test_dataset
 
-    test_dataset = ASLTestDataset(test_data_path, transforms=test_transforms)
+test_dataset = datasets.ImageFolder(test_data_path, transform=validation_transforms)
 
-    columns = 7
-    row = round(len(test_dataset) / columns)
+columns = 7
+row = round(len(test_dataset) / columns)
 
-    fig, ax = plt.subplots(row, columns, figsize=(columns * row, row * columns))
-    plt.subplots_adjust(wspace=0.1, hspace=0.2)
+fig, ax = plt.subplots(row, columns, figsize=(columns * 2, row * 2))
+plt.subplots_adjust(wspace=0.1, hspace=0.2)
 
-    i, j = 0, 0
-    for img, label in test_dataset:
-        img = torch.Tensor(img)
-        img = img.to(device)
-        model.eval()
-        prediction = model(img[None])
+model.eval()
+for idx, (img, label) in enumerate(test_dataset):
+    img = img.to(device)
+    
+    prediction = model(img.unsqueeze(0))
 
-        ax[i][j].imshow(img.cpu().permute(1, 2, 0))
-        ax[i][j].set_title(f'GT {label}. Pred {classes[torch.max(prediction, dim=1)[1]]}')
-        ax[i][j].axis('off')
-        j += 1
-        if j == columns:
-            j = 0
-            i += 1
-            
-    plt.show()
+    ax.flat[idx].imshow(img.cpu().permute(1, 2, 0))
+    ax.flat[idx].set_title(f'GT {label}. Pred {classes[torch.max(prediction, dim=1)[1]]}')
+    ax.flat[idx].axis('off')
+        
+plt.show()
 
 
